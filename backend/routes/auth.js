1622f826a295b649993e243673c90565e4b8a9ec
Istaranjan Das <istaranjandas@gmail.com>
2022-07-15 20:42:48 +0530

    doing the validation and saving data in the db

---
+++ backend/routes/auth.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
+
+router.post('/', [
+    body('name').isLength({min: 3}),
+    body('email').isEmail(),
+    body('password').isLength({min: 5}),
+],(req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }).then(user=> res.json(user));
+        
+})
+
+module.exports = router
